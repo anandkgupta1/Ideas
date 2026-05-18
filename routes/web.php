48d46bc51289c14abd12baf2cca14974262644a3
anandkgupta1 <anandgupta141888@gmail.com>
2026-05-18 16:08:55 +0530

    Add live URL and update README

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 // Dashboard route
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->name('home');
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::post('/follow/{followed_id}', [DashboardController::class, 'followUser'])->middleware('auth');
 
